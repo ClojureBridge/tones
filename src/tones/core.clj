62bdfbe310609eb4bfc0421adc7a497192dba175
David Chelimsky <dchelimsky@gmail.com>
2015-09-06 15:51:33 -0500

    Clean up the example code

--- src/tones/core.clj
+++ src/tones/core.clj
@@ -3,23 +3,23 @@
         [overtone.inst.piano]))
 
 ;; twinkle twinkle little star
-(def phrases1 [:c3 :c3 :g3 :g3 :a3 :a3 :g3])
+(def phrase1 [:c3 :c3 :g3 :g3 :a3 :a3 :g3])
 
 ;; how I wonder what you are
-(def phrases2 ['(:f3 :a3) '(:f3 :a3) '(:e3 :g3) '(:e3 :g3) '(:b2 :d3 ) '(:b2 :d3 ) '(:c3 :e3 :g3)])
+(def phrase2 ['(:f3 :a3) '(:f3 :a3) '(:e3 :g3) '(:e3 :g3) '(:b2 :d3 ) '(:b2 :d3 ) '(:c3 :e3 :g3)])
 
-(defn melody1
-  [start step phrases]
-  (let [time start
-        numbers (map note phrases)]
-    (dorun (map-indexed (fn [i n] (at (+ time (* i step)) (piano n))) numbers))))
+(defn play-note-or-notes [n-or-ns]
+  (if (sequential? n-or-ns)
+    (doseq [n n-or-ns] (piano (note n)))
+    (play-note-or-notes (list n-or-ns))))
 
-(defn melody2
-  [start step phrases]
-  (let [time start
-        chords (fn [p] (doseq [n p] (piano (note n))))]
-    (dotimes [i (count phrases)]
-      (at (+ (* i step) time) (chords (nth phrases i))))))
+(defn play-phrase
+  [start step phrase]
+  (dorun
+   (map-indexed
+    (fn [i n-or-ns]
+      (at (+ start (* i step)) (play-note-or-notes n-or-ns)))
+    phrase)))
 
 (defn twinkle
   "plays a part of Twinkle Little Star.
@@ -28,8 +28,7 @@
   ([time]
       (let [start1 time
             step1 680
-            start2 (+ start1 (* step1 (+ 1 (count phrases1))))
+            start2 (+ start1 (* step1 (inc (count phrase1))))
             step2 640]
-        (melody1 start1 step1 phrases1)
-        (melody2 start2 step2 phrases2))))
-
+        (play-phrase start1 step1 phrase1)
+        (play-phrase start2 step2 phrase2))))
